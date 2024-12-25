2c1eb2e99d91aa4142bd3846bf90afc3c05f5be1
David <st0724@gmail.com>
2024-12-24 23:14:33 -0800

    minor typing tweak for register

--- src/register.tsx
+++ src/register.tsx
@@ -6,13 +6,13 @@ import { h, render, VNode } from "onejs-preact"
 type UObject = CS.UnityEngine.Object
 
 interface Props<T> {
-    target: UObject
+    target: T
     root: Element
 }
 
 export function register<T extends UObject>(type: { new(): T }, Elem: ({ target, root }: Props<T>) => VNode<any>) {
     ___engineHost.RegisterRenderer(puer.$typeof(type), (targetObj, rootVE) => {
         const dom = new DomWrapper(new Dom(rootVE, document._doc));
-        render(<Elem target={targetObj} root={dom} />, dom)
+        render(<Elem target={targetObj as T} root={dom} />, dom)
     })
 }
