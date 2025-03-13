2968b6e626db442de5e0537c81d32a3d79883193
David <st0724@gmail.com>
2025-03-12 17:44:45 -0700

    scratchpad tweak

--- src/scratchpad.tsx
+++ src/scratchpad.tsx
@@ -1,14 +1,33 @@
 import { Dom } from "OneJS/Dom"
-import { DomWrapper } from "onejs-core/dom/dom";
+import { MeshGenerationContext } from "UnityEngine/UIElements"
+import { DomWrapper } from "onejs-core/dom/dom"
 import { h, render, VNode } from "onejs-preact"
+import { useEffect, useRef } from "onejs-preact/hooks"
 
 interface ScratchpadProps {
     window: CS.OneJS.Editor.ScratchpadWindow
 }
 
+const Holder = ({ children }: any) => {
+    const ref = useRef<Element>()
+
+    useEffect(() => {
+        ref.current!.style.width = "100%"
+        ref.current!.style.height = ref.current!.parentNode.ve.resolvedStyle.height - 24
+    }, [])
+
+    return <div ref={ref}>
+        {children}
+    </div>
+}
+
 export function scratchpad(name: string, Elem: ({ window }: ScratchpadProps) => VNode<any>, focus: boolean = false) {
-    var scratchpadWindow = CS.OneJS.Editor.ScratchpadWindow.Get(name, focus);
-    ___engineHost.ApplyStyleSheets(scratchpadWindow.rootVisualElement);
-    const rootDom = new DomWrapper(new Dom(scratchpadWindow.rootVisualElement, document._doc));
-    render(<Elem window={scratchpadWindow} />, rootDom)
+    var scratchpadWindow = CS.OneJS.Editor.ScratchpadWindow.Get(name, focus)
+    ___engineHost.ApplyStyleSheets(scratchpadWindow.rootVisualElement)
+    ___engineHost.add_onReload(() => {
+        if (scratchpadWindow && scratchpadWindow.rootVisualElement)
+            scratchpadWindow.rootVisualElement.Clear()
+    })
+    const rootDom = new DomWrapper(new Dom(scratchpadWindow.rootVisualElement, document._doc))
+    render(<Holder><Elem window={scratchpadWindow} /></Holder>, rootDom)
 }
